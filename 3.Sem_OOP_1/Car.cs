71af724d559d02b0db88ad79e65ad19327893c95
Brezzor <oliv095w@edu.zealand.dk>
2023-02-14 09:34:34 +0100

    Add project files.

---
+++ 3.Sem_OOP_1/Car.cs
@@ -0,0 +1,56 @@
+﻿namespace _3.Sem_OOP_1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public double Price { get; set; }
+        public string? LicensePlate { get; set; }
+
+        public void Validate()
+        {
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+        }
+
+        public void ValidateModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (Model.Length < 4) 
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public void ValidatePrice()
+        {
+            if (Price < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate!.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (LicensePlate!.Length > 7)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Model} {Price} {LicensePlate}";
+        }
+    }
+}
---
+++ 3.Sem_OOP_1Tests/CarTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3.Sem_OOP_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Sem_OOP_1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private readonly Car car = new Car() { Id = 0, Model = "Volvo", Price = 50000, LicensePlate = "AC34175"};
+        private readonly Car carWithNullModel = new Car() { Id = 0, Model = null, Price = 50000, LicensePlate = "AC34175" };
+        private readonly Car carWithEmptyModel = new Car() { Id = 0, Model = "", Price = 50000, LicensePlate = "AC34175" };
+        private readonly Car carWithNegativPrice = new Car() { Id = 0, Model = "Volvo", Price = -50000, LicensePlate = "AC34175" };
+        private readonly Car carWithTooShortLicense = new Car() { Id = 0, Model = "Volvo", Price = 50000, LicensePlate = "A" };
+        private readonly Car carWithTooLongLicense = new Car() { Id = 0, Model = "Volvo", Price = 50000, LicensePlate = "AC341754" };
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentNullException>(() => carWithNullModel.ValidateModel());
+            Assert.ThrowsException<ArgumentException>(() => carWithEmptyModel.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carWithNegativPrice.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carWithTooShortLicense.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carWithTooLongLicense.ValidateLicensePlate());
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = car.ToString();
+            Assert.AreEqual(car.ToString(), str);
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            car.Validate();
+        }
+    }
+}
